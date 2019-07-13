6adeee7be3217c6f1439c4ae4cd5001caf9fe95c
0xZeb <git@github.com/0xZeb>
2019-07-12 20:50:55 -0700

    trying to fix ; error in migration

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,10 +1,21 @@
 module.exports = {
   init(app){
+
     const staticRoutes = require("../routes/static");
+
     const topicRoutes = require("../routes/topics");
+
     const adRoutes = require("../routes/ads");
+
+    const postRoutes = require("../routes/posts");
+
+
     app.use(staticRoutes);
+
     app.use(topicRoutes);
+
     app.use(adRoutes);
+    
+    app.use(postRoutes);
   }
 }
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,5 +1,7 @@
 const Topic = require("./models").Topic;
 
+const Post = require("./models").Post;
+
 module.exports = {
 
   getAllTopics(callback){
@@ -30,6 +32,15 @@ module.exports = {
 
   getTopic(id, callback){
     return Topic.findById(id)
+
+    //being eager load of associated posts -allows us to pull requested objects and associated ones with 1 request, instead of 2.
+    return Topic.findById(id, {
+
+      include: [{
+        model: Post,
+        as: "posts"
+      }]
+    })
     .then((topic) => {
       callback(null, topic);
     })
